72c136925decb6e2da1d8ad7989ee1f5d9457fab
jeb <jevin.st06@gmail.com>
2025-06-06 19:29:50 +0700

    adding basic tsx

---
+++ desktop2_project/src/components/Desktop2Components.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+export const Desktop2_Components = () => 
+{
+    return (
+        <div>
+            Hello world
+        </div>
+    )
+}
+export default Desktop2_Components;
